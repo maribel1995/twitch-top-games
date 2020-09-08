8e039aef84ae3347194511de4d4aba295b66b95f
Mariana Macedo <marianamrs1995@hotmail.com>
2020-09-08 07:23:29 -0300

    adding ico

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,4 @@
 import logo from "assets/twitch-extruded.png";
-import logoHeader from "assets/twitch-icon.png";
 import dotBackground from "assets/dot-background.svg";
 
-export { logo, logoHeader, dotBackground };
+export { logo, dotBackground };
